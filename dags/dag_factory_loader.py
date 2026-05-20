854576416a7f172e8d4149ccc56f65e21b87fd54
Venkat <venkatsai3008@outlook.com>
2026-05-19 19:13:55 -0500

    Implement DAG Factory retail order pipelines with worker queue

---
+++ dags/dag_factory_loader.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from dagfactory import load_yaml_dags
+
+
+config_file = Path("/usr/local/airflow/include/dag_configs/retail_orders.yml")
+
+load_yaml_dags(
+    globals_dict=globals(),
+    config_filepath=str(config_file),
+)
